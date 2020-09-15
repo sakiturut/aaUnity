b1a2d5f85194914d245eba09debd2fe48669ef7b
Muhammet Saki Türüt <msakiturut@gmail.com>
2020-09-15 18:31:25 +0300

    -

--- aa/Assets/Script/CircleControl.cs
+++ aa/Assets/Script/CircleControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CircleControl : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+   
     void Start()
     {
 
@@ -14,22 +14,9 @@ public class CircleControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 1), (float)0.5);
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.name == "circle")
-        {
-           
-            Debug.Log("Circle Collider");
-            GameObject obj;
-            obj = (GameObject)Instantiate(prefab);
-            // child the object to MyShip:
-            obj.transform.position = new Vector3((float)-0.77, (float)-0.24, (float)-0.327);
-            obj.transform.parent = transform;
-        }
-    }
+
 
 }
